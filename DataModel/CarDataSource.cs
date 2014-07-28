f3a573b6064c67a8fb3e36455d92887be691325a
Felipe <felipecembranelli@live.com>
2014-07-28 20:09:06 -0300

    commit1

---
+++ DataModel/CarDataSource.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Storage;
+
+namespace YourCarCost
+{
+    public sealed class FipeDataSource
+    {
+        private static FipeDataSource _fipeDataSource = new FipeDataSource();
+
+        public List<CarModel> CarModels
+        {
+            get { return this._carModels; }
+        }
+        public List<CarBranch> CarBranches
+        {
+            get { return this._carBranches; }
+        }
+
+        public List<CarModelYear> CarModelYears
+        {
+            get { return this._carModelsYears; }
+        }
+
+        //public List<CarBranch> CarBranches
+        //{
+        //    get { return this._carBranches; }
+        //}
+
+        private List<CarModel> _carModels = new List<CarModel>();
+        private List<CarBranch> _carBranches = new List<CarBranch>();
+        private List<CarModelYear> _carModelsYears = new List<CarModelYear>();
+
+        //public async Task GetLocalCarModelsAsync()
+        //{
+        //    if (this._carModels.Count != 0)
+        //        return;
+
+        //    Uri dataUri = new Uri("ms-appx:///DataModel/CarModelsData.json");
+
+        //    StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(dataUri);
+        //    string jsonText = await FileIO.ReadTextAsync(file);
+        //    JsonObject jsonObject = JsonObject.Parse(jsonText);
+        //    JsonArray jsonArray = jsonObject["Cars"].GetArray();
+
+        //    foreach (JsonValue carValue in jsonArray)
+        //    {
+        //        JsonObject carObject = carValue.GetObject();
+        //        CarModel car = new CarModel(carObject["UniqueId"].GetString(),
+        //                                                    carObject["Title"].GetString());
+
+        //        this.CarModels.Add(car);
+        //    }
+        //}
+
+        /// <summary>
+        /// Retorna as marcas de carros
+        /// GET: http://fipeapi.appspot.com/api/1/carros/marcas.json
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<CarBranch>> GetCarBranches()
+        {
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+            Uri resourceUri;
+            string jsonText;
+            List<CarBranch> carBranches = new List<CarBranch>();
+
+            resourceUri = new Uri(" http://fipeapi.appspot.com/api/1/carros/marcas.json");
+
+            Windows.Storage.Streams.IInputStream response = await httpClient.GetInputStreamAsync(resourceUri);
+
+
+            using (StreamReader reader = new StreamReader(response.AsStreamForRead()))
+            {
+                jsonText = await reader.ReadToEndAsync();
+            }
+
+            
+            JsonValue jsonObject = JsonValue.Parse(jsonText);
+
+            JsonArray jsonArray = jsonObject.GetArray();
+
+            foreach (JsonValue carBranchValue in jsonArray)
+            {
+                JsonObject branchObject = carBranchValue.GetObject();
+                CarBranch branch = new CarBranch(branchObject["key"].GetString(),
+                                                            branchObject["id"].GetNumber(),
+                                                            branchObject["fipe_name"].GetString(),
+                                                            branchObject["name"].GetString());
+
+                carBranches.Add(branch);
+            }
+
+            return carBranches;
+        }
+
+
+        /// <summary>
+        /// Retorna lista de modelos de uma determinada marca
+        /// GET: http://fipeapi.appspot.com/api/1/carros/veiculos/21.json
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<CarModel>> GetModelByBranch(string branchId)
+        {
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+            Uri resourceUri;
+            string jsonText;
+            List<CarModel> carModels = new List<CarModel>();
+
+
+            string uri = string.Format("http://fipeapi.appspot.com/api/1/carros/veiculos/{0}.json", branchId);
+
+            resourceUri = new Uri(uri);
+
+            Windows.Storage.Streams.IInputStream response = await httpClient.GetInputStreamAsync(resourceUri);
+
+
+            using (StreamReader reader = new StreamReader(response.AsStreamForRead()))
+            {
+                jsonText = await reader.ReadToEndAsync();
+            }
+
+
+            JsonValue jsonObject = JsonValue.Parse(jsonText);
+
+            JsonArray jsonArray = jsonObject.GetArray();
+
+            foreach (JsonValue item in jsonArray)
+            {
+                JsonObject jObj = item.GetObject();
+                CarModel model = new CarModel(jObj["id"].GetString(),
+                                                jObj["name"].GetString(),
+                                                jObj["key"].GetString(),
+                                                jObj["marca"].GetString(),
+                                                jObj["fipe_name"].GetString(),
+                                                jObj["fipe_marca"].GetString());
+
+                carModels.Add(model);
+            }
+
+            return carModels;
+        }
+
+        /// <summary>
+        /// Retorna os modelos e anos disponíveis para um determinado veículo
+        /// GET: http://fipeapi.appspot.com/api/1/carros/veiculo/21/001267-0.json
+        /// </summary>
+        /// <param name="branchId"></param>
+        /// <returns></returns>
+        public async Task<List<CarModelYear>> GetVersion(string branchId, string carId)
+        {
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+            Uri resourceUri;
+            string jsonText;
+            List<CarModelYear> carModelYears = new List<CarModelYear>();
+
+
+            string uri = string.Format("http://fipeapi.appspot.com/api/1/carros/veiculo/{0}/{1}.json", branchId, carId);
+
+            resourceUri = new Uri(uri);
+
+            Windows.Storage.Streams.IInputStream response = await httpClient.GetInputStreamAsync(resourceUri);
+
+
+            using (StreamReader reader = new StreamReader(response.AsStreamForRead()))
+            {
+                jsonText = await reader.ReadToEndAsync();
+            }
+
+
+            JsonValue jsonObject = JsonValue.Parse(jsonText);
+
+            JsonArray jsonArray = jsonObject.GetArray();
+
+            foreach (JsonValue item in jsonArray)
+            {
+                JsonObject jObj = item.GetObject();
+                CarModelYear modelYears = new CarModelYear(jObj["id"].GetString(),
+                                                jObj["veiculo"].GetString(),
+                                                jObj["key"].GetString(),
+                                                jObj["marca"].GetString(),
+                                                jObj["name"].GetString(),
+                                                jObj["fipe_codigo"].GetString(),
+                                                jObj["fipe_marca"].GetString());
+
+                carModelYears.Add(modelYears);
+            }
+
+            return carModelYears;
+        }
+
+
+    }
+
+
+    /// <summary>
+    /// Modelo
+    /// </summary>
+    public class CarModel
+    {
+        public CarModel(string id, string name, string key, string branch, string fipe_name, string fipe_marca)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.key = key;
+            this.Branch = branch;
+            this.Fipe_name = fipe_name;
+            this.Fipe_marca = fipe_marca;
+
+        }
+
+        public string Id { get; private set; }
+        public string Name{ get; private set; }
+
+        public string key { get; private set; }
+
+        public string Branch { get; set; }
+
+        public string Fipe_name { get; set; }
+
+        public string Fipe_marca { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+
+
+    /// <summary>
+    /// Modelo x Ano
+    /// </summary>
+    public class CarModelYear
+    {
+        public CarModelYear(string id, string veiculo, string key, string marca, string name, string fipe_codigo, string fipe_marca)
+        {
+            this.Id = id;
+            this.Veiculo = veiculo;
+            this.Key = key;
+            this.Marca = marca;
+            this.Name = name;
+            this.Fipe_codigo = fipe_codigo;
+            this.Fipe_marca = fipe_marca;
+
+        }
+        public string Id { get; private set; }
+
+        public string Veiculo { get; private set; }
+        public string Key { get; private set; }
+
+        public string Marca { get; private set; }
+
+        public string Name { get; set; }
+
+        public string Fipe_codigo { get; set; }
+
+        public string Fipe_marca { get; set; }
+
+     
+    }
+
+    /// <summary>
+    /// Marca
+    /// </summary>
+    public class CarBranch
+    {
+
+        public CarBranch(string key, double id, string fipe_name, string name)
+        {
+            this.Key = key;
+            this.Id = id;
+            this.Fipe_name = fipe_name;
+            this.Name = name;
+        }
+        public string Key { get; private set; }
+        public double Id { get; private set; }
+
+        public string Fipe_name { get; private set; }
+
+        public string Name { get; private set; }
+
+    }
+}
---
+++ Home.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using YourCarCost.Data;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace YourCarCost
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Home : Page
+    {
+        List<string> years = new List<string>
+        {
+            "2014","2013","2012"
+        };
+
+        public Home()
+        {
+
+            this.InitializeComponent();
+            this.LoadData();
+        }
+
+
+        private async void ClearFilterLists()
+        {
+
+            //await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, this.cboCarBranch.Items.Clear);
+            //await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, this.cboCarModel.Items.Clear);
+            //await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, this.cboVersion.Items.Clear);
+            
+
+        }
+        private async void LoadData()
+        {
+            FipeDataSource c = new FipeDataSource();
+
+            this.ClearFilterLists();
+
+            var carBranches = await c.GetCarBranches();
+
+            //var carModels = await c.GetCarByBranch(carBranches.FirstOrDefault().Id.ToString());
+
+            //var carDetails = await c.GetModelsAndYearsByCar(carBranches.FirstOrDefault().Id.ToString(), 
+            //                                                carModels.FirstOrDefault().Id.ToString());
+
+            //var carmodel = c.GetCarModelsAsync();
+            this.cboCarBranch.ItemsSource = carBranches;
+
+            //this.cboYear.ItemsSource = years;
+
+        }
+
+        private async void LoadModel()
+        {
+            FipeDataSource c = new FipeDataSource();
+
+            //await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, this.cboCarModel.Items.Clear);
+            //await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, this.cboVersion.Items.Clear);
+
+            if (this.cboCarBranch.SelectedValue != null)
+            {
+                string selectedBranch = (this.cboCarBranch.SelectedValue as CarBranch).Id.ToString();
+
+                var carModels = await c.GetModelByBranch(selectedBranch);
+
+                this.cboCarModel.ItemsSource = carModels;
+
+                
+            }
+        }
+
+        private async void LoadVersion()
+        {
+            FipeDataSource c = new FipeDataSource();
+            string selectedBranch = string.Empty;
+            string selectedModel = string.Empty;
+
+            //await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, this.cboVersion.Items.Clear);
+
+            if ((this.cboCarBranch.SelectedValue != null) && (this.cboCarModel.SelectedValue != null))
+            {
+                selectedBranch = (this.cboCarBranch.SelectedValue as CarBranch).Id.ToString();
+
+                selectedModel = (this.cboCarModel.SelectedValue as CarModel).Id.ToString();
+
+                var carVersions = await c.GetVersion(selectedBranch, selectedModel);
+
+                this.cboVersion.ItemsSource = carVersions;
+            }
+        }
+
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            
+
+        }
+
+        private void cboCarBranch_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.cboCarModel.Items.Clear();
+            this.LoadModel();
+            
+            
+
+        }
+
+        private void cboCarModel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.cboVersion.Items.Clear();
+            this.LoadVersion();
+        }
+
+    }
+}
