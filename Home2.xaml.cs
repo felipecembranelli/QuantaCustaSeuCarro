bf9e3a700e098f26b3774a57ba12f0b7b25aad6e
Felipe <felipecembranelli@live.com>
2014-08-02 10:45:26 -0300

    sharing feature

--- Home2.xaml.cs
+++ Home2.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,6 +23,9 @@ namespace YourCarCost
     /// </summary>
     public sealed partial class Home2 : Page
     {
+        private DataTransferManager dataTransferManager;
+        public const string MissingTitleError = "Entre um título para o que você está compartilhando.";
+
         public Home2()
         {
             this.InitializeComponent();
@@ -36,6 +40,10 @@ namespace YourCarCost
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            // Register the current page as a share source.
+            this.dataTransferManager = DataTransferManager.GetForCurrentView();
+            this.dataTransferManager.DataRequested += new TypedEventHandler<DataTransferManager, DataRequestedEventArgs>(this.OnDataRequested);
+
         }
 
         #region Custom Code
@@ -57,10 +65,12 @@ namespace YourCarCost
                         StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
                         break;
                     case NotifyType.ErrorMessage:
+                        StatusBorder.Height = 100;
                         StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Red);
                         break;
                     case NotifyType.HelpMessage:
-                        StatusBorder.Height = 200;
+                        StatusBorder.Height = 100;
+                        StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
                         break;
                 }
                 StatusBlock.Text = strMessage;
@@ -81,7 +91,9 @@ namespace YourCarCost
         private void ClearNotification()
         {
             StatusBlock.Text = string.Empty;
-            StatusBorder.Height = 20;
+            StatusBorder.Height = 50;
+            StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+            StatusBorder.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
         }
 
         private void ClearFields()
@@ -158,7 +170,10 @@ namespace YourCarCost
 
                 this.cboVersion.ItemsSource = carVersions;
 
-                this.ClearNotification();
+                if (carVersions.Count() == 0)
+                    this.NotifyUser("Dados não disponíveis no site da FIPE.", NotifyType.ErrorMessage);
+                else
+                    this.ClearNotification();
             }
 
            
@@ -249,12 +264,112 @@ namespace YourCarCost
 
         private void Image_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            this.NotifyUser("testando...", NotifyType.HelpMessage);
+            
         }
 
         private void StatusBorder_Tapped(object sender, TappedRoutedEventArgs e)
         {
             this.ClearNotification();
         }
+
+        private void imgHelpFipe_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.NotifyUser("Preço médio da tabela FIPE.", NotifyType.HelpMessage);
+        }
+
+        private void imgHelpIpva_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.NotifyUser("Valor médio de IPVA pago anualmente.", NotifyType.HelpMessage);
+        }
+
+        private void imgHelpSeguro_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.NotifyUser("Valor médio do seguro do carro pago anualmente.", NotifyType.HelpMessage);
+        }
+
+        private void imgHelpManutencao_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.NotifyUser("Valor médio de gastos com manutenção anual do carro.", NotifyType.HelpMessage);
+        }
+
+        private void imgHelpDepreciacao_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.NotifyUser("Valor médio de desvalorização anual do carro.", NotifyType.HelpMessage);
+        }
+
+        private void imgHelpCustoOportunidade_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.NotifyUser("Valor que você poderia estar economizando se tivesse investido o mesmo valor do carro em outra alternativa de investimento, como por exemplo, na poupança (0,5% ao mês).", NotifyType.HelpMessage);
+        }
+
+        private void imgHelpTotalAnual_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.NotifyUser("Valor anual de todos os custos do seu carro.", NotifyType.HelpMessage);
+        }
+
+        private void imgHelpTotalMensal_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.NotifyUser("Valor mensal de todos os custos do seu carro.", NotifyType.HelpMessage);
+        }
+
+        private void Share_Click(object sender, RoutedEventArgs e)
+        {
+            // If the user clicks the share button, invoke the share flow programatically.
+            DataTransferManager.ShowShareUI();
+        }
+
+        private void OnDataRequested(DataTransferManager sender, DataRequestedEventArgs e)
+        {
+            // Call the scenario specific function to populate the datapackage with the data to be shared.
+            if (GetShareContent(e.Request))
+            {
+                // Out of the datapackage properties, the title is required. If the scenario completed successfully, we need
+                // to make sure the title is valid since the sample scenario gets the title from the user.
+                if (String.IsNullOrEmpty(e.Request.Data.Properties.Title))
+                {
+                    e.Request.FailWithDisplayText(MissingTitleError);
+                }
+            }
+        }
+
+        private bool GetShareContent(DataRequest request)
+        {
+            bool succeeded = false;
+
+            // The URI used in this sample is provided by the user so we need to ensure it's a well formatted absolute URI
+            // before we try to share it.
+            this.NotifyUser("", NotifyType.StatusMessage);
+            //Uri dataPackageUri = ValidateAndGetUri(this.adress);
+            if (this.txtTotalMensal.Text == string.Empty)
+            {
+                DataPackage requestData = request.Data;
+                requestData.Properties.Title = "Quanto custa o seu Carro?";
+                requestData.Properties.Description = "Resultado do cálculo de quanto custa o seu carro"; // The description is optional.
+                //requestData.Properties.ContentSourceApplicationLink = ApplicationLink;
+                //requestData.SetWebLink(dataPackageUri);
+
+                //string carro = string.Format("{0} - {1} - {2}",
+                //                this.cboCarBranch.SelectedValue.ToString(),
+                //                this.cboCarModel.SelectedValue.ToString(),
+                //                this.cboVersion.SelectedValue.ToString());
+
+                string carro = string.Format("{0}",
+                                this.cboCarModel.SelectedValue.ToString());
+
+
+                requestData.SetText(string.Format("O seu carro ({0}) está custando para você {1} / mês.",carro, this.txtTotalMensal.Text));
+                //requestData.SetHtmlFormat()
+                succeeded = true;
+            }
+            else
+            {
+                this.NotifyUser("Nenhum carro selecionado para compartilhar o cálculo.", NotifyType.ErrorMessage);
+                //request.FailWithDisplayText("Nenhum carro selecionado para compartilhar o cálculo.");
+            }
+            return succeeded;
+        }
+
+   
     }
+
 }
